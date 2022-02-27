d5a1a513f9eeedaf0d9a35a8903c73a96a7e8f7f
Emmanuel Alabi <bobslegend795@gmail.com>
2022-02-27 20:34:36 +0100

    2.0.0

--- src/__tests__/useContactForm.test.ts
+++ src/__tests__/useContactForm.test.ts
@@ -10,30 +10,30 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe("useContactForm hook", () => {
-  test("success", async () => {
-    const mockFunction = jest.fn();
-    const errFunction = jest.fn();
-    const { result } = renderHook(() =>
-      useContactForm({
-        url: "/api/test",
-        onSuccess: mockFunction,
-        onError: errFunction,
-        companyEmailAddress: "beantech.designs@gmail.com",
-      }),
-    );
+    test("success", async () => {
+        const mockFunction = jest.fn();
+        const errFunction = jest.fn();
+        const { result } = renderHook(() =>
+            useContactForm({
+                url: "/api/test",
+                onSuccess: mockFunction,
+                onError: errFunction,
+                companyEmailAddress: "beantech.designs@gmail.com",
+            }),
+        );
 
-    await act(async () => {
-      result.current.form.setValues({
-        email: "beantech.designs@gmail.com",
-        fullName: "Beantech",
-        subject: "Testing",
-        message: "Testing message",
-      });
+        await act(async () => {
+            result.current.form.setValues({
+                email: "beantech.designs@gmail.com",
+                fullName: "Beantech",
+                subject: "Testing",
+                message: "Testing message",
+            });
 
-      await result.current.form.submitForm();
+            await result.current.form.submitForm();
+        });
+
+        expect(result.current.form.submitCount).toBe(1);
+        expect(result.current.isSending).toBe(false);
     });
-
-    expect(result.current.form.submitCount).toBe(1);
-    expect(result.current.isSending).toBe(false);
-  });
 });
--- src/hooks/useContactForm.ts
+++ src/hooks/useContactForm.ts
@@ -20,91 +20,91 @@ type Options = {
 };
 
 const useContactForm = ({
-  url,
-  companyEmailAddress,
-  onSuccess,
-  onError,
-  template,
+    url,
+    companyEmailAddress,
+    onSuccess,
+    onError,
+    template,
 }: Options) => {
-  const [isSending, setIsSending] = useState<boolean>(false);
+    const [isSending, setIsSending] = useState<boolean>(false);
 
-  const form = useFormik<{
+    const form = useFormik<{
     email: string;
     fullName: string;
     subject: string;
     message: string;
   }>({
-    initialValues: {
-      email: "",
-      fullName: "",
-      subject: "",
-      message: "",
-    },
-    validationSchema: Yup.object({
-      fullName: Yup.string().required("Your fullname is required."),
-      email: Yup.string()
-        .email("Invalid email address.")
-        .required("Your email address is required."),
-      subject: Yup.string().required("Field is required."),
-      message: Yup.string().required("Please input a message."),
-    }),
-    onSubmit: async ({ email, fullName, subject, message }, { resetForm }) => {
-      setIsSending(true);
-      const schema = Yup.object({
-        url: Yup.string().url("Invalid url").required("URL is required"),
-        email: Yup.string()
-          .email("Company emmail address is invalid.")
-          .required("Company email address is required."),
-      });
-      try {
-        await schema.validate({
-          url,
-          email: companyEmailAddress,
-        });
+      initialValues: {
+          email: "",
+          fullName: "",
+          subject: "",
+          message: "",
+      },
+      validationSchema: Yup.object({
+          fullName: Yup.string().required("Your fullname is required."),
+          email: Yup.string()
+              .email("Invalid email address.")
+              .required("Your email address is required."),
+          subject: Yup.string().required("Field is required."),
+          message: Yup.string().required("Please input a message."),
+      }),
+      onSubmit: async ({ email, fullName, subject, message }, { resetForm }) => {
+          setIsSending(true);
+          const schema = Yup.object({
+              url: Yup.string().url("Invalid url").required("URL is required"),
+              email: Yup.string()
+                  .email("Company emmail address is invalid.")
+                  .required("Company email address is required."),
+          });
+          try {
+              await schema.validate({
+                  url,
+                  email: companyEmailAddress,
+              });
 
-        const { data } = await axios({
-          method: "POST",
-          url,
-          data: {
-            to: [companyEmailAddress],
-            subject,
-            body: template
-              ? template(email, fullName, message)
-              : `<div>
+              const { data } = await axios({
+                  method: "POST",
+                  url,
+                  data: {
+                      to: [companyEmailAddress],
+                      subject,
+                      body: template
+                          ? template(email, fullName, message)
+                          : `<div>
                       <p>From: ${fullName}(<strong>${email}</strong>)</p>
                       <div>${message}</div>
                     </div>`,
-          },
-        });
-        setIsSending(false);
-        resetForm();
-        onSuccess(data);
-      } catch (error) {
-        setIsSending(false);
-        if (error?.response) {
-          return onError({
-            ...error?.response?.data,
-          });
-        }
+                  },
+              });
+              setIsSending(false);
+              resetForm();
+              onSuccess(data);
+          } catch (error) {
+              setIsSending(false);
+              if (error?.response) {
+                  return onError({
+                      ...error?.response?.data,
+                  });
+              }
 
-        if (error?.errors) {
-          return onError({
-            ...error,
-            message: error?.errors.join("/n"),
-          });
-        }
+              if (error?.errors) {
+                  return onError({
+                      ...error,
+                      message: error?.errors.join("/n"),
+                  });
+              }
 
-        return onError({
-          message: "Something went wrong please try again",
-        });
-      }
-    },
+              return onError({
+                  message: "Something went wrong please try again",
+              });
+          }
+      },
   });
 
-  return {
-    form,
-    isSending,
-  };
+    return {
+        form,
+        isSending,
+    };
 };
 
 export default useContactForm;
